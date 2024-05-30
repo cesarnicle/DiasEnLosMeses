45022c66ac5dfdc8c4518da2913a150f0592b09c
cesarnicle <cesar.nicle@gmail.com>
2024-05-29 21:51:40 -0300

    dias

---
+++ main.js
@@ -0,0 +1,42 @@
+let mes = prompt ("Ingrese un mes en minuscula");
+
+switch (mes) {
+  case "enero":
+    alert ("Este mes tiene 31 dias");
+    break;
+  case "febrero":
+    alert ("Este mes tiene 28 dias");
+    break;
+  case "marzo":
+    alert ("Este mes tiene 31 dias");
+    break;
+    case "abril":
+        alert ("Este mes tiene 30 dias");
+    break;
+    case "mayo":
+        alert ("Este mes tiene 31 dias");
+    break;
+    case "junio":
+        alert ("Este mes tiene 30 dias");
+    break;
+    case "julio":
+        alert ("Este mes tiene 31 dias");
+    break;
+    case "agosto":
+        alert ("Este mes tiene 31 dias");
+    break;
+    case "setiembre":
+        alert ("Este mes tiene 30 dias");
+    break;
+    case "octubre":
+        alert ("Este mes tiene 31 dias");
+    break;
+    case "noviembre":
+        alert ("Este mes tiene 30 dias");
+    break;
+    case "diciembre":
+        alert ("Este mes tiene 31 dias");
+    break;
+  default:
+    alert ("Eso no es un mes válido");
+}
